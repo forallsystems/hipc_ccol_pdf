5ee3ec16fdfade3cbb50f1ead4f4bfbba828d54d
Toby Kavukattu <kavukattu@gmail.com>
2016-03-22 08:49:15 -0500

    Added Event model

--- backend/api/models.py
+++ backend/api/models.py
@@ -13,3 +13,20 @@ class Flyer(models.Model):
     grade = models.IntegerField()
     subject = models.CharField(max_length=256)
     
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    organizer = models.CharField(max_length=256, blank=True, null=True)
+    website = models.TextField(blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    start_time = models.CharField(max_length=256, blank=True, null=True)
+    end_time = models.CharField(max_length=256, blank=True, null=True)
+    cost = models.CharField(max_length=256, blank=True, null=True)
+    venue_name = models.CharField(max_length=256, blank=True, null=True)
+    venue_street_address= models.CharField(max_length=256, blank=True, null=True)
+    venue_city = models.CharField(max_length=256, blank=True, null=True)
+    venue_state = models.CharField(max_length=256, blank=True, null=True)
+    venue_zipcode = models.CharField(max_length=256, blank=True, null=True)
+    
